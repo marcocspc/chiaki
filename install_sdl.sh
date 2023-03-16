dfb0f433dbd0df6cd6bd2241612fcc1e74051dd9
fredrum <fred.sund@gmail.com>
2023-03-15 20:20:03 -0700

    upgrade for ps5 controller

--- install_sdl.sh
+++ install_sdl.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 echo ""
-echo "This script will install SDL 2.0.20 from the Official github release onto your system."
+echo "This script will install SDL 2.0.22 from the Official github release onto your system."
 echo "Read this script for more details."
 echo ""
 read -r -p "Go ahead? [Y/n] " input
 
 case $input in
       [yY][eE][sS]|[yY])
-            echo "Installing from https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.0.20.zip"
+            echo "Installing from https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.0.22.zip"
             echo ""
             cd third-party/
-            wget https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.0.20.zip
-            unzip release-2.0.20.zip
-	    cd SDL-release-2.0.20
+            wget https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.0.22.zip
+            unzip release-2.0.22.zip
+	    cd SDL-release-2.0.22
 	    sudo apt install libgbm-dev
             ./configure --enable-video-kmsdrm --enable-video-opengles
 	    make -j3
 	    sudo make install
 	    echo ""
-	    echo "SDL 2.0.20 install finished"
+	    echo "SDL 2.0.22 install finished"
 	    echo ""
             ;;
       [nN][oO]|[nN])
