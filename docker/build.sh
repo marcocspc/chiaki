251fc9a2b35740dff0a3e929adcf14244443d172
marcocspc <marcocspc@hotmail.com>
2023-12-19 22:21:49 -0300

    add: build script

---
+++ docker/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$(which docker)" == "" ]]; then
+    echo "docker not found, please install docker"
+    exit 1
+else
+    echo "docker found"
+fi
+
+docker build . -t chiaki
+container=$(docker create chiaki)
+mkdir -p out
+docker cp $container:/app/chiaki-rpi ./out/
+docker rm $container
