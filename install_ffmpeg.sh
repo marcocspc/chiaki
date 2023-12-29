1fd66f76d366248100db77dba4a040ef30ac8112
marcocspc <marcocspc@io.shadow.local>
2023-12-28 22:36:46 -0300

    fix: ffmpeg compilation

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -13,17 +13,20 @@ case $input in
 	echo "Installing from https://github.com/hbiyik/FFmpeg"
 	echo ""
 	cd third-party/
-	sudo apt-get install -y build-essential 
-	sudo apt-get install -y meson libepoxy-dev libxcb-dri3-dev libxcb1-dev libx11-dev libx11-xcb-dev libdrm-dev
-	sudo apt-get install -y libudev-dev
-	sudo apt-get install -y libglfw3-dev libgles2-mesa-dev libepoxy-dev
+	sudo apt-get install -y build-essential \
+                            meson libepoxy-dev libxcb-dri3-dev libxcb1-dev libx11-dev libx11-xcb-dev libdrm-dev \
+                            libudev-dev \
+                            libglfw3-dev libgles2-mesa-dev libepoxy-dev \
+                            libglfw3-dev libgles2-mesa-dev libepoxy-dev \
+                            libaom-dev
+
 	#git clone --branch  test/4.3.5/rpi_main https://github.com/jc-kynesim/rpi-ffmpeg.git
 	git clone --branch  exp_refactor_all https://github.com/hbiyik/FFmpeg
 	cd FFmpeg
 	export CPPFLAGS="-I/usr/include/libdrm"
 	#./configure --enable-shared --disable-static --enable-sand --enable-v4l2-request --enable-libdrm --enable-libudev --enable-opengl --enable-epoxy --enable-vout-egl  --enable-vout-drm
     ./configure --enable-rkmpp --enable-version3 --enable-libdrm --enable-nonfree --enable-gpl --enable-version3 --enable-libx264 --enable-librtmp --enable-shared --enable-static --enable-libx265 --enable-libmp3lame --enable-libpulse --enable-openssl --enable-libopus --enable-libvorbis --enable-libaom --enable-libass --enable-libdav1d --enable-libx265 --enable-libvpx
-	make -j3
+    make -j $(nproc)
 	sudo make install
 	echo ""
 	echo "rkmpp-ffmpeg install finished"
